fb4a5267a24543b84b2cee8d35039b6b3471fa29
FrazierMark <m.mark.frazier@gmail.com>
2022-10-07 14:33:02 -0400

    Still playing with design

--- src/components/Scene.js/Scene.jsx
+++ src/components/Scene.js/Scene.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Sky, Stars } from "@react-three/drei";
+import { OrbitControls, Sky, Stars, Sparkles } from "@react-three/drei";
 import * as THREE from "three";
 import Light from "../Light";
 
 const Scene = () => {
-
-
   return (
     <>
       <Canvas className="canvas" camera={{ position: [0, 100, 195] }}>
         <Light />
         {/* <OrbitControls dampingFactor={0.5} enableDamping="true" /> */}
-        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+        <Stars
+          radius={100}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
+        />
       </Canvas>
     </>
   );
