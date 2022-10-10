a252a409ef8c71e15f371e561d524714f0391f82
FrazierMark <m.mark.frazier@gmail.com>
2022-10-10 10:04:12 -0700

    TV loaded using useLoader

--- src/components/TV.js
+++ src/components/TV.js
@@ -1,7 +1,7 @@
-import { useGLTF } from "@react-three/drei";
-import React from "react"
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function TvModel() {
-    const { nodes } = useGLTF("/tv-v1.glb")
-    return <primitive object={nodes} />
+    const model = useLoader(GLTFLoader, '/tv-v1.glb')
+    return <primitive object={model.scene} />
 }
